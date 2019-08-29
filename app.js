ee3655a35fff38674b76ab5f170c2ffa57ac9e27
Conor Bailey <conorbailey@Conors-MacBook-Pro.local>
2019-08-29 08:29:01 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('Website made by Conor Bailey')
+
+const menuOpen = document.querySelector('.menu-open');
+const menuClose = document.querySelector('.menu-close');
+const nav = document.querySelector('.nav');
+const navLink = document.querySelectorAll(".nav-link");
+const header = document.querySelector('.header');
+
+menuOpen.addEventListener('click', menuToggle);
+menuClose.addEventListener('click', menuToggle);
+
+navLink.forEach(function(elem) {
+    elem.addEventListener("click", menuToggle);
+  });
+
+function menuToggle(){
+    nav.classList.toggle('active')
+    console.log('clicked')
+}
+
+window.onscroll = function(){
+    var top = window.pageYOffset || document.body.scrollTop;
+    if (top > 50) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+    };
