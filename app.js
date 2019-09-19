dd011a0c4b288deea56bc9ca12577d8b336a2ac4
conorbailey90 <conbailey90@gmail.com>
2019-09-19 23:39:07 +0100

    Aniime.js splash screen added

--- app.js
+++ app.js
@@ -5,19 +5,37 @@ const menuClose = document.querySelector('.menu-close');
 const nav = document.querySelector('.nav');
 const navLink = document.querySelectorAll(".nav-link");
 const header = document.querySelector('.header');
+const splash = document.querySelector('.splash');
 
 menuOpen.addEventListener('click', menuToggle);
 menuClose.addEventListener('click', menuToggle);
+
+//Splash Screen
+window.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('hide')}, 2000);
+});
+
+let splashLogo = anime({
+    targets: '.splash-logo',
+    opacity: [0, 100],
+    easing: 'easeInOutExpo',
+    duration: 2000,
+    translateY: window.innerHeight*-0.49,
+    delay: 100,
+    scale: 0.6,
+})
+
 
 navLink.forEach(function(elem) {
     elem.addEventListener("click", menuToggle);
   });
 
 function menuToggle(){
-    nav.classList.toggle('active')
-    console.log('clicked')
+    nav.classList.toggle('active');
 }
 
+// Change Navbar CSS on scroll
 window.onscroll = function(){
     var top = window.pageYOffset || document.body.scrollTop;
     if (top > 50) {
